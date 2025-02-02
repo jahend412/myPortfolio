36144fee846df1758a6f7cf1ca06042dbd13556d
Josh Endemann <jahend44@gmail.com>
2025-02-02 17:34:38 -0600

    Style:  Added Mobie styling
    Basically changed most of the existing to lg: format and then added a formate for smaller screens as the normal one.

--- my-portfolio/src/components/Hero.jsx
+++ my-portfolio/src/components/Hero.jsx
@@ -1,15 +1,23 @@
+import ProfilePic from '/profile-pic.png';
+
 export default function Hero() {
   return (
-    <div className="p-8 dark:bg-gray-800">
-      <h1 className="pt-8 text-center text-3xl font-bold text-gray-800 dark:text-white">
-        Welcome!
-      </h1>
-      <h1 className="text-center text-3xl font-bold text-gray-800 dark:text-white">
-        My name is Josh
-      </h1>
-      <h3 className="pb-4 text-center text-xl text-gray-800 underline dark:text-white">
-        Web Developer
-      </h3>
+    <div className="flex flex-col border-b-2 border-gray-800 bg-amber-50 p-6 dark:border-white dark:bg-gray-800 lg:flex-row lg:p-12">
+      <div>
+        <h3 className="pt-8 text-left text-2xl font-bold text-red-600 lg:pt-20 lg:text-3xl">
+          Welcome, my name is Josh
+        </h3>
+        <h1 className="pb-8 pt-4 text-left text-4xl text-gray-800 dark:text-white lg:pt-8 lg:text-7xl">
+          I am a Full-Stack Web Developer.
+        </h1>
+      </div>
+      <div className="flex items-center justify-center lg:flex-1">
+        <img
+          src={ProfilePic}
+          alt="profile-pic"
+          className="h-64 w-64 object-cover lg:h-80 lg:w-80"
+        />
+      </div>
     </div>
   );
 }
