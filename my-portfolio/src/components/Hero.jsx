e8c54e7b5138364edc295b7338aff15c95198208
Josh Endemann <jahend44@gmail.com>
2025-02-02 13:13:15 -0600

    Create Hero.jsx
    Create the intro to the portfolio.  I made a simple design declaring who I am and what I do

---
+++ my-portfolio/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+export default function Hero() {
+  return (
+    <div className="p-8 dark:bg-gray-800">
+      <h1 className="pt-8 text-center text-3xl font-bold text-gray-800 dark:text-white">
+        Welcome!
+      </h1>
+      <h1 className="text-center text-3xl font-bold text-gray-800 dark:text-white">
+        My name is Josh
+      </h1>
+      <h3 className="pb-4 text-center text-xl text-gray-800 underline dark:text-white">
+        Web Developer
+      </h3>
+    </div>
+  );
+}
